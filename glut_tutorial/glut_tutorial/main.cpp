8a7a57d3d8fe221b37af19c6ab39f2ebdd91f240
DevSusu <os1742@gmail.com>
2016-03-05 11:28:48 +0900

    first triangle tutorial

---
+++ glut_tutorial/glut_tutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  glut_tutorial
+//
+//  Created by Subin_Choi on 3/5/16.
+//  Copyright © 2016 DevSusu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
